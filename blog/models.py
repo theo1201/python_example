f23c6c81f51a335edf785c5038d2e71a508f2c7d
theo <754173764@qq.com>
2019-09-05 07:30:33 +0800

    first paf

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.core.urlresolvers import reverse
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager,
@@ -32,7 +32,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
-                              
+
     objects = models.Manager() # The default manager.
     published = PublishedManager()  # Our custom manager.
 
@@ -41,3 +41,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[
+                           self.publish.year,
+                           self.publish.strftime('%m'),
+                           self.publish.strftime('%d'), self.slug
+                       ])
